b82d798623bc9b81f263071cd2ee0b8b52b5f05b
Phil Scott <phil.scott@gmail.com>
2021-09-27 22:31:14 -0400

    poc

---
+++ DotNet31/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DotNet31
+{
+    public class Class1
+    {
+        public static void DoIt(ServiceCollection services)
+        {
+            services
+                .AddRazorPages()
+                .AddRazorRuntimeCompilation();
+        }
+    }
+}
---
+++ DotNet31RazorOnDotNet6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using DotNet31;
+using Microsoft.AspNetCore.Mvc.Razor.Compilation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+
+namespace DotNet31RazorOnDotNet6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var services = new ServiceCollection();
+            Class1.DoIt(services);
+            Console.WriteLine(services.First(f => f.ServiceType == typeof(IViewCompilerProvider)).ImplementationType?.FullName);
+        }
+    }
+}
